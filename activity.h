899b9dfb1cc8e40b17a6712b1472aa3862057cc5
Kirk Scheibelhut <kjs@google.com>
2014-02-21 17:28:14 -0800

    Nail down activity DataPoint type

--- activity.h
+++ activity.h
@@ -1,60 +1,45 @@
+#include <stdint.h>
+// TODO ifdef stuff...
 typedef struct {
   //id; // startime
 
-  sport; // what about 'multi'
+  sport; // ENUM ?what about 'multi'
   laps; // many points, always at least one
+  data_points;
   //summary; // can include derived statistics (totalAscent, NP, avg)
 } Activity;
 
 typedef struct {
-  timestamp;
-  latitude;
-  longitude;
-  elevation; // altitude
-
-  heart_rate;
-  cadence;
-  distance; // speed cadence sensor
-  // speed
-  // headwind
-  // slope; (gradient)
-
-  temperature;
-
-  watts;
-  torque; // nm
-  balance;
-
-  notes;
-} Point; // TODO name
+  uint32_t timestamp; // s since Unix Epoch - maybe up to 64 bits?
+  int32_t latitude; // semicircles (?)
+  int32_t longitude; // semicircles (?)
+  uint32_t altitude; // altitude = 5 * m + 500
+  uint32_t distance; // 100 *m
+  uint32_t speed; // 1000* m/s
+  uint16_t power; // watts
+  int16_t grade; // 100 * %
+  uint8_t heart_rate; // bpm
+  uint8_t cadence; // rpm
+  uint8_t lr_balance; // ?
+  int8_t temperature; // C
+} DataPoint;
 
 /*
- double secs, cad, hr, km, kph, nm, watts, alt, lon, lat, headwind, slope, temp, lrbalance;
-
-GPX:
-
-elevation
-timestamp
-lat
-lon
-name
-desc
-
-GPX extension:
-HR
-cadence
-temp
-distance
-run
-sensor
-dataPt
-lap
+ * torque, headwind, calories
+ *
+  uint16_t vertical_oscillation; // 10 * mm,
+  uint16_t stance_time_percent; // 100 * percent,
+  uint16_t stance_time; // 10 * ms,
+  uint8_t left_torque_effectiveness; // 2 * percent,
+  uint8_t right_torque_effectiveness; // 2 * percent,
+  uint8_t left_pedal_smoothness; // 2 * percent,
+  uint8_t right_pedal_smoothness; // 2 * percent,
+  uint8_t combined_pedal_smoothness; // 2 * percent,
+*/
 
 
-sportType
-laps
 
-
+/*
 
 TCX:
 
