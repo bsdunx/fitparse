d668294dd5c42b440d73b06bf0430392e8ad830c
Kirk Scheibelhut <kjs@google.com>
2014-03-06 14:19:39 -0800

    Thinking about command line options

--- client.c
+++ client.c
@@ -24,11 +24,8 @@
  * all output format
  *
  * -o/-output = file name to output (could also just be second name)
- * -c/config = config file (has user data and options to pass to things like csv
- *write)
  *
  * input format not required - can always tell or guess
- *
  *
  * --merge - takes two files
  * --split - needs a timestamp... (or distance?)
@@ -41,12 +38,76 @@
  *
  *  also reads from stdin and outputs to stdout by default
  *
+ * -c/config = config file (has user data and options to pass to things like csv
+ *write)
+ * --gender=m,f --units=metric,imperial (u) --hrmax=205  --ftp=300
+ *
+ *
+ *  --help (?), --version
+ *
  */
+
+
 
 int main(int argc, char *argv[]) {
 
-  /*static struct option long_options[] = { };*/
+  ProgramOptions options;
+
+  static struct option long_options[] = {
+    {"help", required_argument, NULL, '?'},
+    {"version", required_argument, NULL, 'v'},
+
+    {"output", required_argument, NULL, 'o'},
+    {"config", required_argument, NULL, 'c'},
+
+    {"csv", no_argument, &options.csv, true},
+    {"gpx", no_argument, &options.gpx, true},
+    {"tcx", no_argument, &options.fit, true},
+    {"fit", no_argument, &options.tcx, true},
+
+    {"summary", no_argument, &options.summary, true},
+    {"laps", no_argument, &options.laps, true},
+
+    {"merge", no_argument, &options.merge, true},
+    {"split", required_argument, &options.split_, true},
+    {"crop", required_argument, &options.crop_, true},
+
+    {"format", required_argument, &options.format_, true},
+    {"fix", required_argument, &options.fix_, true},
+
+    {"gender", required_argument, &options.gender_, true},
+    {"units", required_argument, &options.units_, true},
+    {"hr", required_argument, &options.hr_, true},
+    {"ftp", required_argument, &options.ftp_, true},
+  };
   /*getopt_long_only(argc, argv,);*/
+
+  // one output file in general
+  // could be multiple outputs - splitting on lap or timestamp? - still, only one output name which is actually output base
+  // multiple inputs though
+
+  if (merge) {
+    // can be one file
+  }
+
+  if (split) {
+    // should be one file
+  }
+
+  if (crop) {
+      // should be one file
+  }
+
+  if (fix) {
+    // can be multiple files
+  }
+
+
+  // otherwise take multiple files and output them into format
+
+
+  // otherwise
+
 
   return 0;
 }
